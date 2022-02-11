80d11edc421168ed57446dbed0900148adebf4d3
bataru88 <64689128+bataru88@users.noreply.github.com>
2022-02-12 01:30:59 +0900

    2

--- src/index.js
+++ src/index.js
@@ -18,3 +18,21 @@ const message2 = `私の名前は${name}です`;
 console.log(message2);
 
 //アロー関数
+function func1(str) {
+  str = str + "oooooo";
+  return str;
+}
+console.log(func1("aaaaaaa"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+
+console.log(func2("aaaaa"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func3(10, 20));
